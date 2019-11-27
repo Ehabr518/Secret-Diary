2a834da10243ff95919edc6a84da9cbd6a9b8558
Ehabr518 <Ehabr518@gmail.com>
2019-11-27 19:59:16 +0300

    initial

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("shareddb-r.hosting.stackcp.net", "secretDiary-313233575b", "Or'T,fy0YV>a", "secretDiary-313233575b");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
